b18d78a9545c56615dd353232032a4dc011ad3e6
Olivier Rogier <github.com@ordisoftware.org>
2022-03-27 14:54:31 +0200

    Refactor

--- Project/Properties/AssemblyInfo.cs
+++ Project/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Resources;
+using System.Runtime.InteropServices;
 
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
@@ -32,3 +33,4 @@
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
